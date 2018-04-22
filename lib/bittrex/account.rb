4641a21e6310f27bcde58fa7c6679b667afcbc3b
ducan <ducan2408@gmail.com>
2018-04-22 13:59:08 +0700

    Use guard and Response fails

--- lib/bittrex/account.rb
+++ lib/bittrex/account.rb
@@ -1,14 +1,29 @@
 module Bittrex
   module Account
-    def self.get_account(currency = "BTC")
-      response = Bittrex::Net.get('account/getbalance', { currency: currency.upcase } )
-      Bittrex::Mapping.to_account(response["result"]) if (response && response["success"] == true)
-    end
+    class << self
+      def get_account(currency = "BTC")
+        response = Bittrex::Net.get('account/getbalance', { currency: currency.upcase } )
+        result, error_response = guard(response)
+        return error_response unless result
+        Bittrex::Mapping.to_account(response["result"])
+      end
 
-    def self.get_accounts
-      response = Bittrex::Net.get('account/getbalances')
-      return unless (response && response["success"] == true)
-      response["result"].map{ |acc| Bittrex::Mapping.to_account acc }
+      def get_accounts
+        response = Bittrex::Net.get('account/getbalances')
+        result, error_response = guard(response)
+        return error_response unless result
+        response["result"].map{ |acc| Bittrex::Mapping.to_account acc }
+      end
+
+      def guard(response)
+        if response.nil?
+          [false, ResponseFail.new(success: false, errors: "Nothing return in response")]
+        elsif response["success"] == false
+          [false, ResponseFail.new(success: response["success"], errors: response["message"])]
+        else
+          true
+        end
+      end
     end
   end
 end
